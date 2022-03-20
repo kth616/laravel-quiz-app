5851107aaf99415d873dd58923ddf8bd2066fa1d
kth616 <nitraid.0106@gmail.com>
2022-03-20 14:25:39 +0900

    追加処理と削除処理のコントローラの追加

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    //
+    public function store(Request $request){
+        //入寮内容を検証
+
+
+        //データを取得
+
+        return view('quizzes.index'); 
+    }
+
+    public function destroy($id){
+        
+        return json_encode(['message' => 'ID:'.$id.' が削除されるIDです(仮の出力)']);
+    }
+}
+
